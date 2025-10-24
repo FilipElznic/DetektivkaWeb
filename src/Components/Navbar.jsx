d2f06434791fa2c62820250444c00eef6c1c1e00
Filip Elznic <elznicfilip@gmail.com>
2025-10-24 11:57:19 +0200

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,14 +9,22 @@ export default function Navbar() {
 
   return (
     <nav className="w-full py-8">
-      <div className="max-w-7xl mx-auto px-4">
-        <div className="flex items-center justify-between">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between ">
           {/* Home button on the left */}
           <a
             href="#home"
-            className="relative px-8 py-3 bg-gray-200 text-gray-900 font-mono text-sm uppercase tracking-wide hover:bg-gray-300 transition-colors"
+            className="relative px-12 py-5 bg-gray-200 border-[2px] border-black text-gray-900 font-mono text-xl tracking-wide hover:bg-gray-300 transition-colors"
           >
-            Domů
+            {/* Top-left corner */}
+            <span className="absolute -top-2 -left-2 w-4 h-4 border-l-4 border-t-4 border-white"></span>
+            {/* Top-right corner */}
+            <span className="absolute -top-2 -right-2 w-4 h-4 border-r-4 border-t-4 border-white"></span>
+            {/* Bottom-left corner */}
+            <span className="absolute -bottom-2 -left-2 w-4 h-4 border-l-4 border-b-4 border-white"></span>
+            {/* Bottom-right corner */}
+            <span className="absolute -bottom-2 -right-2 w-4 h-4 border-r-4 border-b-4 border-white"></span>
+            Domu
           </a>
 
           {/* Other navigation items on the right */}
@@ -25,8 +33,17 @@ export default function Navbar() {
               <a
                 key={index}
                 href={item.href}
-                className="relative px-8 py-3 bg-gray-200  text-gray-900 font-mono text-sm uppercase tracking-wide hover:bg-gray-300 transition-colors"
+                className="relative px-12 py-5 bg-gray-200 border-[2px] border-black text-gray-900 font-mono text-xl tracking-wide hover:bg-gray-300 transition-colors"
               >
+                {/* Top-left corner */}
+                <span className="absolute -top-2 -left-2 w-4 h-4 border-l-4 border-t-4 border-white"></span>
+                {/* Top-right corner */}
+                <span className="absolute -top-2 -right-2 w-4 h-4 border-r-4 border-t-4 border-white"></span>
+                {/* Bottom-left corner */}
+                <span className="absolute -bottom-2 -left-2 w-4 h-4 border-l-4 border-b-4 border-white"></span>
+                {/* Bottom-right corner */}
+                <span className="absolute -bottom-2 -right-2 w-4 h-4 border-r-4 border-b-4 border-white"></span>
+
                 {item.label}
               </a>
             ))}
